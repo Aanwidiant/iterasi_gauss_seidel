ba7660aaa3c87e1bdf64d0d701be3b51fe50e321
Aan Widianto <118367298+Aanwidiant@users.noreply.github.com>
2023-09-28 20:18:29 +0700

    Create spl_gauss_seidel.py
    menyelesaikan sistem persamaan dengan metode gauss-seidel

---
+++ spl_gauss_seidel.py
@@ -0,0 +1,32 @@
+# persiapan fungsi
+f1 = lambda x, y, z: (27 - y + 2 * z) / 10
+f2 = lambda x, y, z: (-15 - 3 * x + z) / 10
+f3 = lambda x, y, z: (25 - 2 * x + 3 * y) / 10
+
+# inisialisasi
+x0 = 0
+y0 = 0
+z0 = 0
+iterasi = 1
+
+e = float(input("Masukkan galat yang diinginkan: "))
+print("\niterasi\t\tx\t\ty\t\tz\n")
+kondisi = True
+
+while kondisi:
+    x1 = f1(x0, y0, z0)
+    y1 = f2(x1, y0, z0)
+    z1 = f3(x1, y1, z0)
+    print("%d\t\t%0.4f\t\t%0.4f\t\t%0.4f" % (iterasi, x1, y1, z1))
+    e1 = abs(x0 - x1)
+    e2 = abs(y0 - y1)
+    e3 = abs(z0 - z1)
+
+    iterasi += 1
+    x0 = x1
+    y0 = y1
+    z0 = z1
+
+    kondisi = e1 > e and e2 > e and e3 > e
+
+print("\nHasil x=%0.3f, y=%0.3f, dan z=%0.3f\n" % (x1, y1, z1))
